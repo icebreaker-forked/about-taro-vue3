4ec4b5b1b7130e159dd617b51427bbf480af4bcc
wangzhifeng <229298746@qq.com>
2023-07-13 18:58:34 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <nut-tabs
+    v-model="state.tab4value"
+    title-scroll
+    title-gutter="10"
+    name="tab4value"
+  >
+    <nut-tab-pane v-for="item in state.list4" :title="'Tab ' + item">
+      Tab {{ item }}
+    </nut-tab-pane>
+  </nut-tabs>
+</template>
+<script lang="ts">
+import { reactive } from 'vue';
+export default {
+  setup() {
+    const state = reactive({
+      tab4value: '0',
+      list4: Array.from(new Array(10).keys())
+    });
+    return { state };
+  }
+};
+</script>
